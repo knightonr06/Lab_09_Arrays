13233f8466540bc415b48314f74c4f2028386fd7
knightonr06 <knightonr06@gmail.com>
2025-11-03 23:14:39 -0500

    Update Task7.java

--- src/Task7.java
+++ src/Task7.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Task7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+
+        int[] dataPoints = new int[100];
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1;
+        }
+
+        int userValue = SafeInput.getRangedInt(scanner, "Enter an int between 1 and 100 to search for: ", 1, 100);
+
+        int foundIndex = -1;
+        for (int i = 0; i < dataPoints.length; i++) {
+            if (dataPoints[i] == userValue) {
+                foundIndex = i;
+                break;
+            }
+        }
+
+        if (foundIndex >= 0) {
+            System.out.printf("The value %d was found at array index %d.%n", userValue, foundIndex);
+        } else {
+            System.out.printf("The value %d was not found in the array.%n", userValue);
+        }
+    }
 }
