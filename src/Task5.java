bde2278940c0dd0d103d4e043f07a40d814b4dcb
knightonr06 <knightonr06@gmail.com>
2025-11-03 23:13:47 -0500

    Update Task5.java

--- src/Task5.java
+++ src/Task5.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Task5 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int userValue;
+
+        do {
+            System.out.print("Enter a number between 1 and 100: ");
+            userValue = in.nextInt();
+        } while (userValue < 1 || userValue > 100);
+
+        System.out.println("You entered: " + userValue);
+    }
 }
