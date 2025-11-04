01ad2086a46f3c47d391ad14cf7a6ce6d3c864be
knightonr06 <knightonr06@gmail.com>
2025-11-03 23:10:07 -0500

    Update Task2.java

--- src/Task2.java
+++ src/Task2.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class Task2 {
+    public static void main(String[] args) {
+        int[] dataPoints = new int[100];
+        Random rand = new Random();
+
+        // Regular for loop
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1; // random number 1–100
+        }
+
+        // Optional: print them out to check
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.println( i + " = " + dataPoints[i]);
+        }
+    }
 }
