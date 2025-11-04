8276cffcb0a36ea29df5a4b51863c65680d5cf06
knightonr06 <knightonr06@gmail.com>
2025-11-03 23:13:06 -0500

    Update Task3.java

--- src/Task3.java
+++ src/Task3.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class Task3 {
+    public static void main(String[] args) {
+        int[] dataPoints = new int[100];
+        Random rand = new Random();
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1; // random number 1–100
+        }
+
+        // Print all values on the same line separated by " | "
+        for (int i = 0; i < dataPoints.length; i++) {
+            System.out.print(dataPoints[i]);
+            if (i < dataPoints.length - 1) {
+                System.out.print(" | ");
+            }
+        }
+        System.out.println();
+    }
 }
