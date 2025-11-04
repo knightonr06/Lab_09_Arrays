54599eb1be50c3bb073dd69b175c47df4792c771
knightonr06 <knightonr06@gmail.com>
2025-11-03 23:15:03 -0500

    Update Task8.java

--- src/Task8.java
+++ src/Task8.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Random;
 
 public class Task8 {
+    public static void main(String[] args) {
+        int[] dataPoints = new int[100];
+        Random rand = new Random();
+
+        for (int i = 0; i < dataPoints.length; i++) {
+            dataPoints[i] = rand.nextInt(100) + 1;
+        }
+
+        int min = dataPoints[0];
+        int max = dataPoints[0];
+
+        for (int i = 1; i < dataPoints.length; i++) {
+            int v = dataPoints[i];
+            if (v < min) min = v;
+            if (v > max) max = v;
+        }
+
+        System.out.printf("Minimum value in the array: %d%n", min);
+        System.out.printf("Maximum value in the array: %d%n", max);
+    }
 }
+
